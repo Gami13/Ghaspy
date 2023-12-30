b4e85cd8aed483f0379b34905a911eb40af6e633
SeKaFi69 <92319044+SeKaFi69@users.noreply.github.com>
2023-12-30 01:05:17 +0100

    template sketch

--- client_web/src/App.tsx
+++ client_web/src/App.tsx
@@ -1,35 +1,64 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import style from './App.module.css';
+import { Home, Search, List, User, Bookmark, Mail, Bell, Settings, Users, MoreHorizontalIcon } from 'lucide-solid';
+import './assets/logo.png';
 function App() {
-  const [count, setCount] = createSignal(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<div class={style.main}>
+				<header>
+					<a href="" class={style.logo}></a>{' '}
+					<a href="">
+						{/* <Home /> */}
+						<h3>Home</h3>
+					</a>
+					<a href="">
+						{/* <Search /> */}
+						<h3>Explore</h3>
+					</a>
+					<a href="">
+						{/* <Bell /> */}
+						<h3>Notifications</h3>
+					</a>
+					<a href="">
+						{/* <Mail /> */}
+						<h3>Messages</h3>
+					</a>
+					<a href="">
+						{/* <List /> */}
+						<h3>Lists</h3>
+					</a>
+					<a href="">
+						{/* <Bookmark /> */}
+						<h3>Bookmarks</h3>
+					</a>
+					<a href="">
+						{/* <Users /> */}
+						<h3>Communities</h3>
+					</a>
+					<a href="">
+						{/* <User /> */}
+						<h3>Profile</h3>
+					</a>
+					<a href="">
+						{/* <MoreHorizontalIcon /> */}
+						<h3>More</h3>
+					</a>
+					<a href="" class={style.post}>
+						Post
+					</a>
+					<div class={style.user_data}></div>
+				</header>
+				<div class={style.content}>
+					<div class={style.content_header}>
+						<button>For You</button>
+						<button>Following</button>
+						<button>.</button>
+					</div>
+				</div>
+				<div class={style.banner}></div>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
