1bf94548ba1f126326cdfab1f2d22e403568080e
SeKaFi69 <92319044+SeKaFi69@users.noreply.github.com>
2024-01-08 18:26:11 +0100

    posty

--- client_web/src/components/Main/Main.tsx
+++ client_web/src/components/Main/Main.tsx
@@ -1,19 +1,26 @@
 import style from './Main.module.css';
-
+import { Match } from 'solid-js';
 import TrendBar from '../TrendBar/TrendBar';
 import Navbar from '../Navbar/Navbar';
+import { useLocation } from '@solidjs/router';
 
 import { JSXElement } from 'solid-js';
 type MainProps = {
 	children: Element | Element[] | JSXElement | JSXElement[];
 };
 function Main(props: MainProps) {
+	const location = useLocation();
+	console.log(location);
 	return (
 		<div class={style.main}>
 			<Navbar className={style.navbar} />
 			{/* <PostBar className={style.postbar} /> */}
-			<main class={[style.mainBar].join(' ')}>{props.children}</main>
-			<TrendBar className={style.trendbar} />
+			<main class={[style.mainBar, location.pathname.toString() === '/signup' ? style.noBanner : ''].join(' ')}>
+				{props.children}
+			</main>
+			<Match when={location.pathname.toString() === '/signup'}>
+				<TrendBar className={style.trendbar} />
+			</Match>
 		</div>
 	);
 }
