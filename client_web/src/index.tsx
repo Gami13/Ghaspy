bccdaae5674deeb9f17ab4c18e27e83998f76611
Gami <gamiofficial0@gmail.com>
2024-03-08 18:24:56 +0100

    css default

--- client_web/src/index.tsx
+++ client_web/src/index.tsx
@@ -7,26 +7,30 @@ import './index.css';
 
 import { AppStateProvider, useAppState } from './AppState';
 
-import { onMount } from 'solid-js';
-import { API_URL } from './constants';
-
 import * as stylex from '@stylexjs/stylex';
 import { Main } from './components/Main';
 
 const styles = stylex.create({
 	root: {
-		width: '100%',
-
-		backgroundColor: 'red',
+		fontFamily: 'Arial, sans-serif',
+		width: '100vw',
+		height: '100vh',
+		margin: 0,
+		padding: 0,
+		backgroundColor: 'black',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
 	},
 });
 
 const root = document.getElementsByTagName('body')[0];
+
 function App() {
 	const AppState = useAppState();
 
 	return (
-		<div {...stylex.props(styles.root)}>
+		<div {...stylex.attrs(styles.root)}>
 			<AppStateProvider>
 				<Router>
 					<Route path="/" component={Main}></Route>
