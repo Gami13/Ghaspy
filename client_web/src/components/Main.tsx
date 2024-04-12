008a75e96fc1b8c657629ed1b9fed12fa7a749ec
Marcin Czechowicz <gamiofficial0@gmail.com>
2024-04-12 12:20:52 +0200

    wip

--- client_web/src/components/Main.tsx
+++ client_web/src/components/Main.tsx
@@ -1,9 +1,7 @@
 import stylex from "@stylexjs/stylex";
-import { Post } from "./Post/Post";
-import type { Post as PostType } from "@/types/internal";
 import { colors } from "../variables.stylex";
 import { posts } from "@/MockData";
-import { For } from "solid-js";
+import { createVirtualizer } from "@tanstack/solid-virtual";
 
 const styles = stylex.create({
 	main: {
@@ -18,11 +16,63 @@ const styles = stylex.create({
 		overflowY: "auto",
 	},
 });
+const randomNumber = (min: number, max: number) =>
+	Math.floor(Math.random() * (max - min + 1) + min);
+
+const sentences = new Array(10000).fill(true).map(() => {
+	const sentence = posts[randomNumber(0, posts.length - 1)].content;
+	return sentence;
+});
 
 export function Main() {
+	let parentRef!: HTMLDivElement;
+	const count = sentences.length;
+	const virtualizer = createVirtualizer({
+		count,
+		getScrollElement: () => parentRef,
+		estimateSize: () => 90,
+		overscan: 5,
+	});
+
+	const items = virtualizer.getVirtualItems();
+
 	return (
-		<div {...stylex.attrs(styles.main)}>
-			<For each={posts}>{(post: PostType) => <Post post={post} />}</For>
+		<div
+			// {...stylex.attrs(styles.main)}
+			ref={parentRef}
+			class="List"
+			style={{ height: "100vh", width: "400px", "overflow-y": "auto" }}
+		>
+			<div
+				style={{
+					height: `${virtualizer.getTotalSize()}px`,
+					width: "100%",
+					position: "relative",
+				}}
+			>
+				<div
+					style={{
+						position: "absolute",
+						top: 0,
+						left: 0,
+						width: "100%",
+						transform: `translateY(${items[0].start ? items[0].start : 0}px)`,
+					}}
+				>
+					{items.map((virtualRow) => (
+						<div
+							data-index={virtualRow.index}
+							ref={(el) => queueMicrotask(() => virtualizer.measureElement(el))}
+							class={virtualRow.index % 2 ? "ListItemOdd" : "ListItemEven"}
+						>
+							<div style={{ padding: "10px 0" }}>
+								<div>Row {virtualRow.index}</div>
+								<div>{sentences[virtualRow.index]}</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
 		</div>
 	);
 }
