e9d5d522c0d93aa27f24550cb7a9d00c47faf367
Gami <gamiofficial0@gmail.com>
2024-04-23 22:53:20 +0200

    rename

--- client_web/src/Translation.tsx
+++ client_web/src/Translation.tsx
@@ -27,30 +27,30 @@ export function formatDate(date: string): string {
 }
 
 const SECOND = 1000;
-const SECONDS = SECOND * 1.5;
+const SECONDS_THRESHOLD = SECOND * 1.5;
 const MINUTE = 60 * SECOND;
-const MINUTES = MINUTE * 1.5;
+const MINUTES_THRESHOLD = MINUTE * 1.5;
 const HOUR = 60 * MINUTE;
-const HOURS = HOUR * 1.5;
+const HOURS_THRESHOLD = HOUR * 1.5;
 const DAY = 24 * HOUR;
-const DAYS = DAY * 1.5;
+const DAYS_THRESHOLD = DAY * 1.5;
 const MONTH = 30 * DAY;
-const MONTHS = MONTH * 1.5;
+const MONTHS_THRESHOLD = MONTH * 1.5;
 const YEAR = 12 * MONTH;
-const YEARS = YEAR * 1.5;
+const YEARS_THRESHOLD = YEAR * 1.5;
 
 export function timeSince(date: string): string {
 	const difference = new Date().getTime() - new Date(date).getTime();
-	if (difference >= YEARS) return t.relativeTime.past({ ago: t.relativeTime.yy({ x: Math.floor(difference / YEAR) }) });
+	if (difference >= YEARS_THRESHOLD) return t.relativeTime.past({ ago: t.relativeTime.yy({ x: Math.floor(difference / YEAR) }) });
 	if (difference >= YEAR) return t.relativeTime.past({ ago: t.relativeTime.y({ x: 1 }) });
-	if (difference >= MONTHS) return t.relativeTime.past({ ago: t.relativeTime.MM({ x: Math.floor(difference / MONTH) }) });
+	if (difference >= MONTHS_THRESHOLD) return t.relativeTime.past({ ago: t.relativeTime.MM({ x: Math.floor(difference / MONTH) }) });
 	if (difference >= MONTH) return t.relativeTime.past({ ago: t.relativeTime.M({ x: 1 }) });
-	if (difference >= DAYS) return t.relativeTime.past({ ago: t.relativeTime.dd({ x: Math.floor(difference / DAY) }) });
+	if (difference >= DAYS_THRESHOLD) return t.relativeTime.past({ ago: t.relativeTime.dd({ x: Math.floor(difference / DAY) }) });
 	if (difference >= DAY) return t.relativeTime.past({ ago: t.relativeTime.d({ x: 1 }) });
-	if (difference >= HOURS) return t.relativeTime.past({ ago: t.relativeTime.hh({ x: difference / HOUR }) });
+	if (difference >= HOURS_THRESHOLD) return t.relativeTime.past({ ago: t.relativeTime.hh({ x: difference / HOUR }) });
 	if (difference >= HOUR) return t.relativeTime.past({ ago: t.relativeTime.h({ x: 1 }) });
-	if (difference >= MINUTES) return t.relativeTime.past({ ago: t.relativeTime.mm({ x: Math.floor(difference / MINUTE) }) });
+	if (difference >= MINUTES_THRESHOLD) return t.relativeTime.past({ ago: t.relativeTime.mm({ x: Math.floor(difference / MINUTE) }) });
 	if (difference >= MINUTE) return t.relativeTime.past({ ago: t.relativeTime.m({ x: 1 }) });
-	if (difference >= SECONDS) return t.relativeTime.past({ ago: t.relativeTime.ss({ x: Math.floor(difference / SECOND) }) });
-	return t.relativeTime.past({ ago: t.relativeTime.s({ x: 1 }) });
+	if (difference >= SECONDS_THRESHOLD) return t.relativeTime.past({ ago: t.relativeTime.ss({ x: Math.floor(difference / SECOND) }) });
+	return t.relativeTime.past({ ago: t.relativeTime.s({ x: Math.floor(difference / SECOND) }) });
 }
