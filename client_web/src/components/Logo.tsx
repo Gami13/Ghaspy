38580e9e840548f7d35237065da3989d9261c4bc
SeKaFi <joker13313@interia.pl>
2024-05-22 10:05:30 +0200

    poprawka

--- client_web/src/components/Logo.tsx
+++ client_web/src/components/Logo.tsx
@@ -8,65 +8,14 @@ import logo128xSmoke from "../Logos/logo128xSmoke.svg";
 
 import stylex from "@stylexjs/stylex";
 
-const infinitySymbolMove = stylex.keyframes({
-	// "0%": { left: "0", top: "0" },
-	// "6.25%": { left: "25%", top: "25%" },
-	// "12.5%": { left: "50%", top: "0" },
-	// "18.75%": { left: "25%", top: "-25%" },
-	// "25%": { left: "0", top: "0" },
-	// "31.25%": { left: "-25%", top: "25%" },
-	// "37.5%": { left: "-50%", top: "0" },
-	// "43.75%": { left: "-25%", top: "-25%" },
-	// "50%": { left: "0", top: "0" },
-	// "56.25%": { left: "25%", top: "25%" },
-	// "62.5%": { left: "0", top: "50%" },
-	// "68.75%": { left: "-25%", top: "25%" },
-	// "75%": { left: "0", top: "0" },
-	// "81.25%": { left: "25%", top: "-25%" },
-	// "87.5%": { left: "0", top: "-50%" },
-	// "93.75%": { left: "-25%", top: "-25%" },
-	// "100%": { left: "0", top: "0" },
-	// center it
-	"0%": { left: "50%", top: "50%" },
-	"6.25%": { left: "75%", top: "25%" },
-	"12.5%": { left: "100%", top: "50%" },
-	"18.75%": { left: "75%", top: "75%" },
-	"25%": { left: "50%", top: "50%" },
-	"31.25%": { left: "25%", top: "25%" },
-	"37.5%": { left: "0%", top: "50%" },
-	"43.75%": { left: "25%", top: "75%" },
-	"50%": { left: "50%", top: "50%" },
-	"56.25%": { left: "75%", top: "25%" },
-
-	"62.5%": { left: "50%", top: "0%" },
-	"68.75%": { left: "25%", top: "25%" },
-	"75%": { left: "50%", top: "50%" },
-	"81.25%": { left: "75%", top: "75%" },
-	"87.5%": { left: "50%", top: "100%" },
-	"93.75%": { left: "25%", top: "75%" },
-
-	"100%": { left: "50%", top: "50%" },
-});
 const styles = stylex.create({
 	logoFace: {
 		position: "absolute",
-		animationName: infinitySymbolMove,
-		animationDuration: "5s",
-		animationIterationCount: "infinite",
-		animationTimingFunction: "linear",
-		animationDelay: "0s",
+
 		zIndex: 10,
-		outline: "red 1px solid",
-		transform: "translate(-50%, -50%)",
 	},
 	logoSmoke: {
 		position: "absolute",
-		animationName: infinitySymbolMove,
-		animationDuration: "5s",
-		animationIterationCount: "infinite",
-		animationTimingFunction: "linear",
-		animationDelay: "0s",
-		transform: "translate(-50%, -50%)",
 	},
 });
 
